cba8528216ebf64a84257c28aa4d2214223bb75e
7heaven <caifangmao8@gmail.com>
2016-05-11 19:35:57 +0800

    first commit

---
+++ SHSoftwareRasterizer/decoders/IDecoder.h
@@ -0,0 +1,23 @@
+//
+//  IDecoder.h
+//  SHSoftwareRasterizer
+//
+//  Created by 7heaven on 16/5/11.
+//  Copyright © 2016年 7heaven. All rights reserved.
+//
+
+#ifndef IDecoder_h
+#define IDecoder_h
+
+#include <string.h>
+
+namespace sh{
+class IDecoder{
+public:
+    virtual std::string getFileType() = 0;
+    
+    virtual ~IDecoder();
+}
+}
+
+#endif
